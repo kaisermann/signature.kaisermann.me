1322550c5f2f60f1750de0920c2a44ab5d934b10
Christian Kaisermann <christian@kaisermann.me>
2017-06-21 17:43:41 -0300

    Small refactor

--- ksig.js
+++ ksig.js
@@ -1,1 +1,54 @@
-!function(){function a(a){var b=parseInt(window.getComputedStyle(a).height),c=a.getBoundingClientRect(),d=document.documentElement;return c.top>=-1*b&&c.bottom<=(window.innerHeight||d.clientHeight)+b}function l(a,b,c){var d;return function(){var e=this,f=arguments,g=function(){d=null,c||a.apply(e,f)},h=c&&!d;clearTimeout(d),d=setTimeout(g,b),h&&a.apply(e,f)}}!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/BlackBerry/i,h=/Opera Mini/i,i=/IEMobile/i,j=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,k=RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),l=function(a,b){return a.test(b)},m=function(a){var m=a||navigator.userAgent;this.apple={phone:l(b,m),ipod:l(c,m),tablet:l(d,m),device:l(b,m)||l(c,m)||l(d,m)},this.android={phone:l(e,m),tablet:!l(e,m)&&l(f,m),device:l(e,m)||l(f,m)},this.other={blackberry:l(g,m),opera:l(h,m),windows:l(i,m),firefox:l(j,m),device:l(g,m)||l(h,m)||l(i,m)||l(j,m)},this.seven_inch=l(k,m),this.any=this.apple.device||this.android.device||this.other.device||this.seven_inch},n=a.isMobile=new m;n.Class=m}(window);var b="https://kaisermann.me/",c="https://signature.kaisermann.me/",d="ksig.css",e={bc:"#353535",tc:"#fff",position:"right",state:"static",bottomoffset:50,responsive:"false"},f=document.head||document.querySelector("head")||document.getElementsByTagName("head")[0],g=document.querySelectorAll('script[src*="signature.kaisermann.me"]')[0],h=g.parentNode,i=function(){o(),e.responsive="true"==e.responsive,n(c+d,function(a){m(a),j()})},j=function(){var c=document.createElement("a"),d="k-sig k-"+e.state;if(c.className=d,c.href=b,c.target="_blank",c.innerHTML='<span class="k-sig-by">By</span><div class="k-sig-brand"><span class="k-sig-main">K</span><span class="k-sig-rest">AISERMANN</span></div>',isMobile.any&&e.responsive&&window.addEventListener("scroll",l(function(){var f=(window,document),g=f.documentElement,h=f.body,i=pageYOffset||(g.clientHeight?g.scrollTop:h.scrollTop),j=k(f),l=window.innerHeight;scroll_bottom_offset=l+i,c.className=a(c)&&scroll_bottom_offset>=j-e.bottomoffset?d+" k-sig-visible":d},200)),"static"==e.state)h.insertBefore(c,g);else{var f=document.body;if("bottom"==e.state){var i=f.style.position;"relative"!=i&&"absolute"!=i&&(f.style.position="relative")}f.appendChild(c)}h.removeChild(g)},k=function(a){return Math.max(a.documentElement.clientHeight,a.body.scrollHeight,a.documentElement.scrollHeight,a.body.offsetHeight,a.documentElement.offsetHeight)},m=function(a){var b=document.createElement("style");a=a.replace(/url\(k-path/g,"url("+c+"assets").replace(/\$bc\$/g,e.bc).replace(/\$tc\$/g,e.tc).replace(/\$position\$/g,e.position),b.type="text/css",b.setAttribute("author","Christian Kaisermann <christian@kaisermann.me>"),b.styleSheet?b.styleSheet.cssText=a:b.appendChild(document.createTextNode(a)),f.appendChild(b)},n=function(a,b){var c;c=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP"),c.onreadystatechange=function(){4==c.readyState&&200==c.status&&b(c.responseText)},c.open("GET",a,!0),c.send()},o=function(){if(g.src.indexOf("#")>=0)for(var a=g.src,b=a.substring(a.indexOf("#")+1),c=b.split("&"),d=0;d<c.length;d++)if(c[d]){var f=c[d].split("=");f.length<1||(e[f[0]]=f[1]?f[1]:null)}};i()}();
+(function(){;(function(i){var e=/iPhone/i,n=/iPod/i,o=/iPad/i,t=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,r=/Android/i,d=/BlackBerry/i,s=/Opera Mini/i,a=/IEMobile/i,b=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,h=RegExp('(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)','i'),c=function(i,e){return i.test(e)},l=function(i){var l=i||navigator.userAgent;(this.apple={phone:c(e,l),ipod:c(n,l),tablet:c(o,l),device:c(e,l)||c(n,l)||c(o,l),}),(this.android={phone:c(t,l),tablet:!c(t,l)&&c(r,l),device:c(t,l)||c(r,l),}),(this.other={blackberry:c(d,l),opera:c(s,l),windows:c(a,l),firefox:c(b,l),device:c(d,l)||c(s,l)||c(a,l)||c(b,l),}),(this.seven_inch=c(h,l)),(this.any=this.apple.device||this.android.device||this.other.device||this.seven_inch)},v=(i.isMobile=new l())
+v.Class=l})(window)
+function isInViewport(a){var b=parseInt(window.getComputedStyle(a).height)
+var c=a.getBoundingClientRect(),d=document.documentElement
+return(c.top>=-1*b&&c.bottom<=(window.innerHeight||d.clientHeight)+b)}
+var mainLink='https://kaisermann.me/'
+var path='https://signature.kaisermann.me/'
+var sigcss='ksig.css'
+var options={bc:'#353535',tc:'#fff',position:'right',state:'static',bottomoffset:50,responsive:'false',}
+var pageHead=document.head||document.getElementsByTagName('head')[0]||document.querySelector('head')
+var scriptElement=document.querySelector('script[src*="signature.kaisermann.me"]')
+var scriptParent=scriptElement.parentNode
+var _init=function(){parseHash()
+options.responsive=options.responsive=='true'
+getFile(path+sigcss,function signatureCssLoaded(cssText){createCSS(cssText)
+createSignature()})}
+var createSignature=function(){var a=document.createElement('a')
+var aClasses='k-sig k-'+options.state
+a.className=aClasses
+a.href=mainLink
+a.target='_blank'
+a.innerHTML='<span class="k-sig-by">By</span><div class="k-sig-brand"><span class="k-sig-main">K</span><span class="k-sig-rest">AISERMANN</span></div>'
+if(isMobile.any&&options.responsive){window.addEventListener('scroll',debounce(function(){var w=window,d=document,e=d.documentElement,g=d.body,scrollTop=pageYOffset||(e.clientHeight?e.scrollTop:g.scrollTop),docHeight=getElemHeight(d),winHeight=window.innerHeight
+var scrollBottomOffset=winHeight+scrollTop
+a.className=isInViewport(a)&&scrollBottomOffset>=docHeight-options.bottomoffset?aClasses+' k-sig-visible':aClasses},200))}
+if(options.state==='static'){scriptParent.insertBefore(a,scriptElement)}else{var body=document.body
+if(options.state==='bottom'){var dp=body.style.position
+if(dp!=='relative'&&dp!=='absolute'){body.style.position='relative'}}
+body.appendChild(a)}
+scriptParent.removeChild(scriptElement)}
+var getElemHeight=function(elem){return Math.max(elem.documentElement.clientHeight,elem.body.scrollHeight,elem.documentElement.scrollHeight,elem.body.offsetHeight,elem.documentElement.offsetHeight)}
+function debounce(func,wait,immediate){var timeout
+return function(){var context=this,args=arguments
+var later=function(){timeout=null
+if(!immediate)func.apply(context,args)}
+var callNow=immediate&&!timeout
+clearTimeout(timeout)
+timeout=setTimeout(later,wait)
+if(callNow)func.apply(context,args)}}
+var createCSS=function(css){var sigStyle=document.createElement('style')
+css=css.replace(/url\(\$kPath\$/g,'url('+path+'assets').replace(/\$bc\$/g,options.bc).replace(/\$tc\$/g,options.tc).replace(/\$position\$/g,options.position)
+sigStyle.type='text/css'
+sigStyle.setAttribute('author','Christian Kaisermann <christian@kaisermann.me>')
+if(sigStyle.styleSheet)sigStyle.styleSheet.cssText=css
+else sigStyle.appendChild(document.createTextNode(css))
+pageHead.appendChild(sigStyle)}
+var getFile=function(a,b){var c
+c=new XMLHttpRequest()
+c.onreadystatechange=function(){c.readyState===4&&c.status===200&&b(c.responseText)}
+c.open('GET',a,!0)
+c.send()}
+var parseHash=function(){if(scriptElement.src.indexOf('#')>=0){for(var a=scriptElement.src,b=a.substring(a.indexOf('#')+1),c=b.split('&'),d=0;d<c.length;d++){if(c[d]){var e=c[d].split('=')
+e.length<1||(options[e[0]]=e[1]?e[1]:null)}}}}
+_init()})()
